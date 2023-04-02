7404bce3ead6f23a121786bf68c112fe34a5d55c
Ajay Keelu <84334673+Deepak-000@users.noreply.github.com>
2023-04-02 12:24:28 +0530

    Add files via upload

--- Movie App/script.js
+++ Movie App/script.js
@@ -3,11 +3,39 @@ let retrive = async ()=>{
     let data = await dataRes.json()
     funcTable(data.results);
 }
+let headerSearch = document.querySelector('#headerSearch')
+let searchButton = document.querySelector('#searchButton');
+searchButton.addEventListener('click',async ()=>{
+    container.innerHTML = '';
+    let dataRes = await fetch(search+headerSearch.value);
+    let data = await dataRes.json()
+    if(data.results.length == 0){
+        let resultsNotFound = document.querySelector('.resultsNotFound');
+        resultsNotFound.innerHTML = `
+        <h1>Results not found for query ' ${headerSearch.value} '</h1>
+        `
+    }
+    else
+    funcTable(data.results); 
+})
+const search = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=';
 retrive()
+
 let container = document.querySelector('.container');
 const funcTable = (data) =>{
-    console.log(data[0])
-    data.forEach(e => {
-        container.innerHTML += `<div class="image"><img src = 'https://image.tmdb.org/t/p/w200${e.poster_path}'><div class="rating">Rating : ${e.vote_average}</div></div>`
+    document.querySelector('.resultsNotFound').innerHTML = ""
+        data.forEach(e => {
+            console.log(e.overview)
+            container.innerHTML += `
+            <div class="image">
+                <img src = 'https://image.tmdb.org/t/p/w200${e.poster_path}' />
+                <div class="rating">
+                    Rating : ${e.vote_average}
+                </div>
+                <div class="desc">
+                    ${e.overview}
+                </div>
+            </div>
+            `
     });
 }
